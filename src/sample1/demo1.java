0aa70fefe04f28823472fa0d4dc1af05ddaacc86
Ojas <Ojas@DESKTOP-ETCNBJR>
2022-07-22 20:25:21 +0530

    [Rohan]: added new class

---
+++ src/sample1/demo1.java
@@ -0,0 +1,15 @@
+package sample1;
+
+public class demo1 
+{
+public static void main(String[] args) 
+{
+	System.out.println("Hii");
+	
+	System.out.println("hello");
+	
+	System.out.println("hello Radhika");
+	
+	System.out.println("love u Radhika");
+}
+}
